2fdf264691a6f738b19c25fe1c92604757b16d26
Zoltan Altfatter <altfatterz@gmail.com>
2019-06-10 22:37:02 +0200

    playing with gateway

---
+++ new-way/new-api-gateway/src/main/java/com/mimacom/lunchandlearn/newapigateway/RouteConfigurations.java
@@ -0,0 +1,33 @@
+package com.mimacom.lunchandlearn.newapigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.util.function.Predicate;
+
+
+/**
+ * Routes: http://localhost:8080/actuator/gateway/routes
+ *
+ * More details here: https://cloud.spring.io/spring-cloud-gateway/multi/multi__actuator_api.html
+ */
+@Configuration
+public class RouteConfigurations {
+
+    @Bean
+    RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("order_service_route",
+                        route -> route.path("/orders")
+                                .and()
+                                .method(HttpMethod.GET)
+                                .and()
+                                .predicate(serverWebExchange ->
+                                        serverWebExchange.getRequest().getQueryParams().containsKey("customerId"))
+                                .uri("lb://new-order-service")).build();
+    }
+}
