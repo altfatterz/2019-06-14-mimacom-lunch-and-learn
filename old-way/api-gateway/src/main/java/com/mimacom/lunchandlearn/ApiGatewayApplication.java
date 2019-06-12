18d8e788f68712cbb3111f7aa17fbec1a5b728fc
Zoltan Altfatter <altfatterz@gmail.com>
2019-06-12 22:49:07 +0200

    add hystrix dashboard example

--- old-way/api-gateway/src/main/java/com/mimacom/lunchandlearn/ApiGatewayApplication.java
+++ old-way/api-gateway/src/main/java/com/mimacom/lunchandlearn/ApiGatewayApplication.java
@@ -2,10 +2,21 @@ package com.mimacom.lunchandlearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableHystrixDashboard // makes the /hystrix available
+
+/**
+ * add the following url: http://localhost:9090/actuator/hystrix.stream
+ *
+ * to make traffic:
+ *
+ * ab -c 5 -n 5000 http://localhost:8080/customer-service/customers/1
+ */
+
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
@@ -13,3 +24,5 @@ public class ApiGatewayApplication {
 	}
 
 }
+
+
